4ca5187718d50e035c04a3b171df917915fcad86
T.Bilguun <bilguun.t@tomyo.mn>
2023-03-22 15:54:57 +0800

    zoos2 bodolt O(n^3)

---
+++ bod12-zoos1/zoos2_Bilguun (n^3).cpp
@@ -0,0 +1,27 @@
+// Bodson: T.Bilguun ( O(n^3) bodolt )
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int n, m, tooluur = 0;
+	cout << "n= ";
+	cin >> n;
+	cout << "m= ";
+	cin >> m;
+	int c[m];
+	for(int i=0; i<m; i++) {
+		cin >> c[i];
+	}
+	
+	for(int i=0; i<m; i++) {
+		for(int k=0; k<m; k++) {
+			for(int j=0; j<m; j++) {
+				if(c[i]+c[k]+c[j] == n) {
+					tooluur++;
+				}
+			}
+		}
+	}
+	cout << "Hariu: " << tooluur;
+}
