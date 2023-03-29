9852b12cb93283a088a2cc24704e901fb69fccf4
Togtuun <robotogtuun@gmail.com>
2023-03-29 15:40:43 +0800

    fixes

--- das01-er-histogram/codes/togtuun_histogram.py
+++ das01-er-histogram/codes/togtuun_histogram.py
@@ -2,7 +2,7 @@ import networkit as nk
 import matplotlib.pyplot as plt
 from time import time
 
-erg = nk.generators.ErdosRenyiGenerator(1000, 0.08)
+erg = nk.generators.ErdosRenyiGenerator(10000, 0.08)
 generated = erg.generate()
 
 values = []
@@ -11,10 +11,15 @@ start = time()
 for node in generated.iterNodes():
     values.append(generated.degree(node))
 
-plt.hist(values)
-plt.title("Erdos Renyi")
-plt.ylabel("Number of degree")
-plt.xlabel("Nodes")
+plt.grid(color='#EEEEEE10')
+_, _, patches = plt.hist(values, 20, rwidth=0.9)
+plt.title("Erdos Renyi /n = 1000, p=0.08/")
+plt.ylabel("Frequency")
+plt.xlabel("Number of degree")
+for pp in patches:
+    x, y = pp.xy
+    plt.text(x, pp.get_height(), pp.get_height(), {'size': 7})
+
 plt.savefig("./image.png")
 
 print(f"{time()-start} milliseconds")
