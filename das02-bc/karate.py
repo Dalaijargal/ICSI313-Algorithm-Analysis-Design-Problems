3a79920934d9fbc49fe581d3eeac27afe28832e0
Baljinnyam dayan <baljinnyam.dayan@gmail.com>
2023-04-12 15:47:25 +0800

    zurag

---
+++ das02-bc/karate.py
@@ -0,0 +1,10 @@
+import networkit as nk
+import file_reader as fr
+import matplotlib.pyplot as plt
+
+G,n=fr.readaspath('benchmark1/karate.txt')
+print(G)
+
+# draw the graph using nk viz
+nk.viztasks.drawGraph(G)
+plt.show()
