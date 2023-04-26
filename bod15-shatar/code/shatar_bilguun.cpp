1e82b81bcea1d7a74808a8b4a8580f04a7f15c67
T.Bilguun <bilguun.t@tomyo.mn>
2023-04-26 17:31:30 +0800

    fixed code

--- bod15-shatar/code/shatar_bilguun.cpp
+++ bod15-shatar/code/shatar_bilguun.cpp
@@ -5,15 +5,18 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    int A[n][n], dp[n][n];
+    vector< vector<int> > A(n), dp(n);
+
+    for(int i=0; i<n; i++) {
+        A[i] = vector<int>(n);
+        dp[i] = vector<int>(n);
+    }
 
     for(int i=0; i<n; i++) {
         for(int j=0; j<n; j++) {
             cin >> A[i][j];
         }
     }
-    
-    memset(dp, 0, sizeof(dp));
 
     for(int i=0; i<n; i++) {
         dp[0][i] = A[0][i];
@@ -32,14 +35,14 @@ int main() {
             }
         }
     }
-    cout << "---------------------" << endl;
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<n; j++) {
-            cout << dp[i][j] << " ";
-        }
-        cout << endl;
-    }
-    cout << "---------------------" << endl;
+    // cout << "---------------------" << endl;
+    // for(int i=0; i<n; i++) {
+    //     for(int j=0; j<n; j++) {
+    //         cout << dp[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+    // cout << "---------------------" << endl;
     int min = 9999;
     for(int j=0; j<n; j++) {
         if(min > dp[n-1][j]) {
