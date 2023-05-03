d4efede674b8720c19e9327a6ce37679fe6345b4
Baljinnyam dayan <baljinnyam.dayan@gmail.com>
2023-05-03 15:00:12 +0800

    test

--- bod15-shatar/code/shatar_baljinnyam.cpp
+++ bod15-shatar/code/shatar_baljinnyam.cpp
@@ -17,24 +17,29 @@ int main() {
   for (int i = 0; i < n; i++) {
     dp[0][i] = input[0][i];
   }
+
   for (int i = 1; i < n; i++) {
     for (int j = 0; j < n; j++) {
-      dp[i][j] = min(dp[i - 1][j] + input[i][j],
-                     min(j - 1 >= 0 ? dp[i - 1][j - 1] : INT_MAX + input[i][j],
-                         j + 1 < n ? dp[i - 1][j + 1] : INT_MAX + input[i][j]));
+      dp[i][j] = dp[i - 1][j] + input[i][j];
+      if (j > 0) {
+        dp[i][j] = min(dp[i][j], dp[i - 1][j - 1] + input[i][j]);
+      }
+      if (j < n - 1) {
+        dp[i][j] = min(dp[i][j], dp[i - 1][j + 1] + input[i][j]);
+      }
     }
   }
   //   print dp
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-      cout << dp[i][j] << " ";
-    }
-    cout << endl;
-  }
+  // for (int i = 0; i < n; i++) {
+  //   for (int j = 0; j < n; j++) {
+  //     cout << dp[i][j] << " ";
+  //   }
+  //   cout << endl;
+  // }
   int result = INT_MAX;
   for (int i = 0; i < n; i++) {
     result = min(result, dp[n - 1][i]);
   }
-  cout << "result : " << result;
+  cout << result;
   return 0;
 }
