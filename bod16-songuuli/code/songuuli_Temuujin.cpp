0095fd4e1681ceedec731f6a062b77c8a3187934
tmjn957 <76428069+tmjn957@users.noreply.github.com>
2023-05-03 15:03:48 +0800

    Add files via upload

---
+++ bod16-songuuli/code/songuuli_Temuujin.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+int dp[100000], e[100000], a[100000];
+int max(int a, int b)
+{
+    return a > b ? a : b;
+}
+
+main()
+{
+    int n;
+    cout << "n:";
+    cin >> n;
+
+    cout << "a[i]\n";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    cout << "\n e[i]\n";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> e[i];
+    }
+
+    dp[0] = a[0];
+    dp[1] = std::max(a[0] + e[1], e[0]+a[1]);
+    dp[2] = std::max(std::max(a[0]+e[1]+e[2], e[0]+a[1]+e[2]), e[0]+e[1]+a[2]);
+
+    for (int i = 3; i < n; i++)
+    {
+        dp[i] = std::max(e[i] + dp[i - 1], a[i] + e[i - 1] + e[i - 2] + dp[i - 3]);
+    }
+    cout << "Erdeniin awch boloh onoo:" << dp[n - 1];
+}
